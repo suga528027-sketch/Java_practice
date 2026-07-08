76062f4080951d3407d121ede6dadcc8dc7e419a
SUGAVANESHWARAN MS <suga528027@gmail.com>
2026-07-08 22:53:53 +0530

    Time: 5 ms (80.04%), Space: 45.6 MB (96.89%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp=x;
+        int rev=0;
+        if(x<0)
+        return false;
+        while(x!=0)
+        {
+            int mod=x%10;
+            rev=rev*10+mod;
+            x/=10;
+
+        }
+        if(rev==temp)
+        return true;
+        else 
+        return false;
+    }
+}
