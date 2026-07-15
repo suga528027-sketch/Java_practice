093f3e0e86f372054b3a7037a040931b56e11bfb
SUGAVANESHWARAN MS <suga528027@gmail.com>
2026-07-15 20:47:03 +0530

    Time: 1 ms (83.11%), Space: 42.5 MB (57.16%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        int sum1 = 1;
+        int sum2 = 2;
+        for(int i=0;i<n;i++)
+        {
+            sum1+=2;
+            sum2+=2;
+        }
+        return n;
+    }
+}
