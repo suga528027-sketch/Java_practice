40ea03f196491f62224106c1df7bda26695921dc
SUGAVANESHWARAN MS <suga528027@gmail.com>
2026-07-30 22:34:21 +0530

    Time: 4 ms (46.51%), Space: 44.6 MB (46.29%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       StringBuilder sb=new StringBuilder();
+       for(int i=0;i<s.length();i++)
+       {
+        char ch=s.charAt(i);
+        if(Character.isLetterOrDigit(ch))
+        {
+            sb.append(Character.toLowerCase(ch));
+        }
+       } 
+       int left=0;
+       int right =sb.length()-1;
+       while(left<right)
+       {
+        if(sb.charAt(left)!=sb.charAt(right))
+        {
+            return false;
+        }
+        left++;
+        right--;
+       }
+       return true;
+    }
+}
