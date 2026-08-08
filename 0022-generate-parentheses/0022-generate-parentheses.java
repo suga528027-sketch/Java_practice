e4faf90f159bf4f273cba23835f407c149aab64f
SUGAVANESHWARAN MS <suga528027@gmail.com>
2026-08-08 13:53:36 +0530

    Time: 1 ms (86.06%), Space: 44.1 MB (96.78%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,28 +1,29 @@
 class Solution {
     List<String > ans = new ArrayList<>();
     public List<String> generateParenthesis(int n) {
-       // StringBuilder sb ;
         backtrack(n , 0 , 0 , new StringBuilder());
         return ans;
     }
+       
     public void backtrack(int n , int open , int close , StringBuilder sb)
     {
-        if(sb.length() == 2 * n)
+        if(sb.length() == 2*n)
         {
             ans.add(sb.toString());
-            return;
         }
+        // open bracket
         if(open < n)
         {
             sb.append("(");
-            backtrack(n , open+1 , close ,sb);
+            backtrack(n , open + 1 , close ,sb);
             sb.deleteCharAt(sb.length() - 1);
         }
-         if(close  < open)
+        // close bracket
+        if(close < open)
         {
             sb.append(")");
-            backtrack(n , open , close+1 ,sb);
-            sb.deleteCharAt(sb.length() - 1);
+            backtrack(n , open , close + 1 , sb);
+            sb.deleteCharAt(sb.length() -1);
         }
     }
 }
