9151c3db3819f57e2ceeb657f09b9d2741185ed2
SUGAVANESHWARAN MS <suga528027@gmail.com>
2026-08-26 23:20:49 +0530

    Time: 0 ms (100%), Space: 42.8 MB (56.99%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==1) return nums[0];
+        int [] arr=new int[nums.length];
+        arr[0]=nums[0];
+        arr[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++)
+        {
+            arr[i]=Math.max(arr[i-2]+nums[i],arr[i-1]);
+        }
+        return arr[arr.length-1];
+    }
+}
